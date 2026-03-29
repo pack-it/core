86e8d88f34aacf340d0cdee103c3f8917dffbb48
Frank <mail@franku.nl>
2026-03-29 22:37:16 +0200

    Add make 4.4

---
+++ packages/make/build.bat
@@ -0,0 +1,5 @@
+@echo off
+
+set INSTALL_DIR=%PACKIT_PACKAGE_PATH%
+
+call build_w32.bat
---
+++ packages/make/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+cd make-$PACKIT_PACKAGE_VERSION
+
+if [[ $PACKIT_TARGET =~ "linux" ]]; then
+    ./configure --prefix=$PACKIT_PACKAGE_PATH
+fi
+
+if [[ $PACKIT_TARGET =~ "apple" ]]; then
+    ./configure --prefix=$PACKIT_PACKAGE_PATH --program-prefix=g
+fi
+
+sh build.sh
