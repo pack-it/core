3ce811008e3c2e6c3d1d852e43dbd446858fff5b
Frank <mail@franku.nl>
2026-03-31 14:17:53 +0200

    Add perl 5.42.1 for unix

---
+++ packages/perl/build.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+cd perl-$PACKIT_PACKAGE_VERSION
+
+active_dir="$PACKIT_PREFIX_PATH/active/perl"
+
+./Configure -des \
+    -Dinstallprefix=$PACKIT_PACKAGE_PATH \
+    -Dprefix=$active_dir \
+    -Dprivlib=$PACKIT_PACKAGE_VERSION/lib/perl5/ \
+    -Dsitelib=$PACKIT_PREFIX_PATH/lib/perl5/site_perl/$PACKIT_PACKAGE_VERSION \
+    -Dvendorlib=$PACKIT_PREFIX_PATH/lib/perl5/vendor_perl/$PACKIT_PACKAGE_VERSION \
+    -Dvendorprefix=$PACKIT_PREFIX_PATH \
+    -Dperlpath=$active_dir/bin/perl \
+    -Dstartperl=#!$active_dir/bin/perl \
+    -Dman1dir=$PACKIT_PACKAGE_PATH/share/man/man1 \
+    -Dman3dir=$PACKIT_PACKAGE_PATH/share/man/man3 \
+    -Duseshrplib \
+    -Duselargefiles \
+    -Dusethreads
+
+make
+
+make test
+
+make install
