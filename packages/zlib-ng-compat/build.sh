24111b31f0da987bf6d6832ac7f4d692792f5c5e
Max Rijkers <max_rijkers1@icloud.com>
2026-04-07 16:30:21 +0200

    set the install name dir to package-prefix/lib

--- packages/zlib-ng-compat/build.sh
+++ packages/zlib-ng-compat/build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 cd zlib-ng-$PACKIT_PACKAGE_VERSION
 
-./configure --prefix=$PACKIT_PACKAGE_PATH --zlib-compat
+./configure --prefix=$PACKIT_PACKAGE_PATH --zlib-compat --installnamedir="$PACKIT_PACKAGE_PATH/lib"
+
 make
 
 make install
