badbb3122b692a5797eea1dbd41b99fecb1d53de
Frank <mail@franku.nl>
2026-04-11 01:12:48 +0200

    Fix perl configure

--- packages/perl/build.sh
+++ packages/perl/build.sh
@@ -6,12 +6,12 @@ active_dir="$PACKIT_PREFIX_PATH/active/perl"
 ./Configure -des \
     -Dinstallprefix=$PACKIT_PACKAGE_PATH \
     -Dprefix=$active_dir \
-    -Dprivlib=$PACKIT_PACKAGE_VERSION/lib/perl5/ \
-    -Dsitelib=$PACKIT_PREFIX_PATH/lib/perl5/site_perl/$PACKIT_PACKAGE_VERSION \
-    -Dvendorlib=$PACKIT_PREFIX_PATH/lib/perl5/vendor_perl/$PACKIT_PACKAGE_VERSION \
+    -Dprivlib=$PACKIT_PACKAGE_PATH/lib/perl5/ \
+    -Dsitelib=$PACKIT_PREFIX_PATH/lib/perl5/site_perl/$PACKIT_ARGS_LIB_VERSION \
+    -Dvendorlib=$PACKIT_PREFIX_PATH/lib/perl5/vendor_perl/$PACKIT_ARGS_LIB_VERSION \
     -Dvendorprefix=$PACKIT_PREFIX_PATH \
     -Dperlpath=$active_dir/bin/perl \
-    -Dstartperl=#!$active_dir/bin/perl \
+    -Dstartperl="#!$active_dir/bin/perl" \
     -Dman1dir=$PACKIT_PACKAGE_PATH/share/man/man1 \
     -Dman3dir=$PACKIT_PACKAGE_PATH/share/man/man3 \
     -Duseshrplib \
