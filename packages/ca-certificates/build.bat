3d2822b80a80f6d71c0c6f776503745bfbb02ccf
Frank <mail@franku.nl>
2026-04-12 23:47:03 +0200

    Add package ca-certificates for windows

---
+++ packages/ca-certificates/build.bat
@@ -0,0 +1,1 @@
+copy cacert-$PACKIT_ARGS_FILE_VERSION.pem "$PACKIT_PACKAGE_PATH\cacert.pem"
---
+++ packages/ca-certificates/postinstall.bat
@@ -0,0 +1,3 @@
+mklink "cert.pem" "cacert.pem"
+
+REM TODO: Load system certificates when openssl is installed
