3934ee49e78baca070999baad15ece9b18b77c14
Max Rijkers <max_rijkers1@icloud.com>
2026-04-15 16:25:48 +0200

    Add libpng for windows

---
+++ packages/libpng/build.bat
@@ -0,0 +1,10 @@
+cd make-%PACKIT_PACKAGE_VERSION%
+
+if %PACKIT_TARGET% "x86_64-pc-windows-msvc" (
+    nmake -f scripts\makefile.vcwin32
+) else if %PACKIT_TARGET% "aarch64-pc-windows-msvc" (
+    nmake -f scripts\makefile.vcwin-arm64
+) else (
+    echo Insupported target, exiting
+    exit /b 1
+)
