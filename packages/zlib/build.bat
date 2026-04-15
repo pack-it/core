efe30b8fb7a2c33c1983663dd337571721a9fd2b
Max <max_rijkers1@icloud.com>
2026-04-15 19:16:17 +0200

    Fix build script and add test.bat

--- packages/zlib/build.bat
+++ packages/zlib/build.bat
@@ -1,5 +1,35 @@
 cd zlib-%PACKIT_PACKAGE_VERSION%
 
+REM Read Visual Studio install path
+for /f "tokens=* usebackq" %%i in (`"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere" -latest -property installationPath`) do (
+    set VSPATH=%%i
+)
+if not exist "%VSPATH%" (
+    echo Visual Studio cannot be loaded from %VSPATH%
+    exit /b 1
+)
+
+REM Check if vcvarsall.bat exists
+set "VCVARSALL=%VSPATH%\VC\Auxiliary\Build\vcvarsall.bat"
+if not exist "%VCVARSALL%" (
+    echo vcvarsall.bat cannot be loaded from %VCVARSALL%
+    exit /b 1
+)
+echo Found vcvarsall.bat at %VCVARSALL%
+
+REM Retrieve architecture from target
+if "%PACKIT_TARGET%"=="x86_64-pc-windows-msvc" (
+    set ARCH=x64
+) else if "%PACKIT_TARGET%"=="aarch64-pc-windows-msvc" (
+    set ARCH=arm64
+) else (
+    echo Target %PACKIT_TARGET% is not supported for this package
+    exit /b 1
+)
+
+REM Call vcvarsall.bat to set MSVC build environment
+call "%VCVARSALL%" %ARCH%
+
 nmake -f win32/Makefile.msc
 
 if not exist zlib.h (
@@ -18,12 +48,12 @@ if not exist zdll.lib (
     exit \b 1
 )
 
-mkdir "%PACKIT_PACKAGES_PATH%\include"
-mkdir "%PACKIT_PACKAGES_PATH%\lib\pkgconfig"
-mkdir "%PACKIT_PACKAGES_PATH%\bin"
+mkdir "%PACKIT_PACKAGE_PATH%\include\"
+mkdir "%PACKIT_PACKAGE_PATH%\lib\pkgconfig\"
+mkdir "%PACKIT_PACKAGE_PATH%\bin\"
 
-move "zlib1.dll" "%PACKIT_PACKAGES_PATH%\bin"
-move "zdll.lib" "%PACKIT_PACKAGES_PATH%\lib"
-move "zlib.h" "%PACKIT_PACKAGES_PATH%\include"
-move "zconf.h" "%PACKIT_PACKAGES_PATH%\include"
-move "zlib.lib" "%PACKIT_PACKAGES_PATH%\lib"
+move "zlib1.dll" "%PACKIT_PACKAGE_PATH%\bin\"
+move "zdll.lib" "%PACKIT_PACKAGE_PATH%\lib\"
+move "zlib.h" "%PACKIT_PACKAGE_PATH%\include\"
+move "zconf.h" "%PACKIT_PACKAGE_PATH%\include\"
+move "zlib.lib" "%PACKIT_PACKAGE_PATH%\lib\"
