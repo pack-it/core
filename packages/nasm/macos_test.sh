8490a40f1ba8c97db581967906215ae4ac7d0b05
Max <maxrijkers@outlook.com>
2026-04-19 17:03:57 +0200

    Fix macos test script

--- packages/nasm/macos_test.sh
+++ packages/nasm/macos_test.sh
@@ -2,12 +2,12 @@
 
 # Hello World example from https://cs.lmu.edu/~ray/notes/nasmtutorial/
 cat << EOF > test.asm
-            global    start
+            global    _start
 
             section   .text
-start:      mov       rax, 0x02000004
+_start:     mov       rax, 0x02000004
             mov       rdi, 1
-            mov       rsi, message
+            lea       rsi, [rel message]
             mov       rdx, 13
             syscall
             mov       rax, 0x02000001
@@ -19,9 +19,18 @@ message:    db        "Hello, World", 10
 EOF
 
 "$PACKIT_PACKAGE_PATH/bin/nasm" -fmacho64 test.asm
-ld test.o
 
-OUTPUT=$(./a.out)
+# Link with ld, pass along the correct SDK version and path
+SDK_VERSION=$(xcrun --sdk macosx --show-sdk-version)
+SDK_PATH=$(xcrun --sdk macosx --show-sdk-path)
+ld test.o \
+  -platform_version macos "$SDK_VERSION" "$SDK_VERSION" \
+  -lSystem \
+  -syslibroot "$SDK_PATH" \
+  -e _start \
+  -o test
+
+OUTPUT=$(./test)
 
 if [ "$OUTPUT" != "Hello, World" ]; then
     exit 1
