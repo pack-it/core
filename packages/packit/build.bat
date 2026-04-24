f26cb502c852758342604688793dbf3ff08deba3
Max <max_rijkers1@icloud.com>
2026-04-24 20:37:09 +0200

    Fix windows build script

--- packages/packit/build.bat
+++ packages/packit/build.bat
@@ -1,4 +1,4 @@
 REM Workaround to include Packit in core repository (Packit cannot yet be build, because Rust is not yet a supported package)
-mkdir "%PACKIT_PACKAGE_PATH%/bin/"
-move "packit@%PACKIT_PACKAGE_VERSION%-0-%PACKIT_TARGET%" "%PACKIT_PACKAGE_PATH%/bin/packit"
-mklink "%PACKIT_PACKAGE_PATH%/bin/pit" "%PACKIT_PACKAGE_PATH%/bin/packit"
+mkdir "%PACKIT_PACKAGE_PATH%\bin\"
+move "packit@%PACKIT_PACKAGE_VERSION%-0-%PACKIT_TARGET%.exe" "%PACKIT_PACKAGE_PATH%\bin\packit.exe"
+mklink "%PACKIT_PACKAGE_PATH%\bin\pit.exe" "%PACKIT_PACKAGE_PATH%\bin\packit.exe"
