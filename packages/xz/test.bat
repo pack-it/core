4ecbca60a7bd23676b2955feb7a13daf82b1dfe0
Frank <mail@franku.nl>
2026-05-31 23:10:48 +0200

    Cleanup test scripts

--- packages/xz/test.bat
+++ packages/xz/test.bat
@@ -2,8 +2,8 @@ set TEST_TEXT = "CMake is finally added as package, so we can add xz!"
 echo "%TEST_TEXT%" > test.txt
 
 REM Compress and decompress to see if information stays the same
-xz -c test.txt > compressed.xz
-xz -dc compressed.xz > decompressed.txt
+"%PACKIT_PACKAGE_PATH%\bin\xz" -c test.txt > compressed.xz
+"%PACKIT_PACKAGE_PATH%\bin\xz" -dc compressed.xz > decompressed.txt
 
 set /p RESULT = < decompressed.txt
 
