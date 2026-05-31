1a5381de7831c44f66a351ac3de255fea035f6d3
Frank <mail@franku.nl>
2026-05-31 20:24:02 +0200

    Use --config Release instead of -DCMAKE_BUILD_TYPE=Release

--- packages/zlib-ng-compat/build.bat
+++ packages/zlib-ng-compat/build.bat
@@ -1,8 +1,8 @@
 cd zlib-ng-%PACKIT_PACKAGE_VERSION%
 
-cmake -S . -B build -DCMAKE_INSTALL_PREFIX="%PACKIT_PACKAGE_PATH%" -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF -DZLIB_COMPAT=ON
+cmake -S . -B build -DCMAKE_INSTALL_PREFIX="%PACKIT_PACKAGE_PATH%" -DBUILD_TESTING=OFF -DZLIB_COMPAT=ON
 
-cmake --build build
+cmake --build build --config Release
 
 ctest --verbose -C Release
 
