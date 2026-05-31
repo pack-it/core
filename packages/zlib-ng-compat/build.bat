30953dacc314b90dcba1a27f1dca00e0cd943167
Frank <mail@franku.nl>
2026-05-31 19:40:45 +0200

    Add support for building zlib-ng and zlib-ng-compat on Windows

---
+++ packages/zlib-ng-compat/build.bat
@@ -0,0 +1,9 @@
+cd zlib-ng-%PACKIT_PACKAGE_VERSION%
+
+cmake -S . -B build "-DCMAKE_INSTALL_PREFIX=%PACKIT_PACKAGE_PATH%" -DCMAKE_BUILD_TYPE=Release -DZLIB_COMPAT=ON
+
+cmake --build build
+
+ctest --verbose -C Release
+
+cmake --install build
---
+++ packages/zlib-ng/build.bat
@@ -0,0 +1,9 @@
+cd zlib-ng-%PACKIT_PACKAGE_VERSION%
+
+cmake -S . -B build "-DCMAKE_INSTALL_PREFIX=%PACKIT_PACKAGE_PATH%" -DCMAKE_BUILD_TYPE=Release
+
+cmake --build build
+
+ctest --verbose -C Release
+
+cmake --install build
