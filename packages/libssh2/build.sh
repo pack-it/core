0b505d493bf0a3bda3ef4c5b9514bb93fe217508
Max <max_rijkers1@icloud.com>
2026-06-01 12:59:33 +0200

    Fix libssh2 for Linux

--- packages/libssh2/build.sh
+++ packages/libssh2/build.sh
@@ -6,6 +6,7 @@ cd libssh2-$PACKIT_PACKAGE_VERSION
     --disable-examples-build \
     --with-openssl \
     --with-libz \
+    --with-libz-prefix="$PACKIT_PACKAGE_DEPENDENCIES_PATH/zlib-ng-compat" \
     --with-libssl-prefix="$PACKIT_PACKAGE_DEPENDENCIES_PATH/openssl"
 
 make
--- packages/libssh2/test.sh
+++ packages/libssh2/test.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-# Compile test.c
-gcc -L "$PACKIT_PACKAGE_PATH/lib" -I "$PACKIT_PACKAGE_PATH/include" test.c -o test -lssh2
+# Compile test.c 
+if [ $PACKIT_OS = "linux" ]; then
+    # Specifically specify the rpath, otherwise libssh2.so.1 cannot be found at runtime
+    gcc -L "$PACKIT_PACKAGE_PATH/lib" -I "$PACKIT_PACKAGE_PATH/include" -Wl,-rpath,"$PACKIT_PACKAGE_PATH/lib" test.c -o test -lssh2
+else
+    gcc -L "$PACKIT_PACKAGE_PATH/lib" -I "$PACKIT_PACKAGE_PATH/include" test.c -o test -lssh2
+fi
 
 # Run the file to see if libssh2 works
 ./test
