9bbaf66e69e1c7ea9a4ed21078ec9ddaa8d9d2d7
Max <87335212+Max13245@users.noreply.github.com>
2026-06-02 23:38:30 +0200

    Add package llhttp (#39)
    * Add llhttp package
    
    * Fix llhttp for Windows

---
+++ packages/llhttp/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+cd llhttp-release-v$PACKIT_PACKAGE_VERSION
+
+cmake -S . -B build -DCMAKE_INSTALL_PREFIX="$PACKIT_PACKAGE_PATH"
+
+cmake --build build --config Release
+
+ctest --verbose -C Release
+
+cmake --install build
