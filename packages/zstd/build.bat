8a5a54f423625fa28f0f70aa99ccdd1ce6543501
Max <max_rijkers1@icloud.com>
2026-06-02 22:27:14 +0200

    Fix zstd for Windows

--- packages/zstd/build.bat
+++ packages/zstd/build.bat
@@ -1,6 +1,6 @@
 cd zstd-%PACKIT_PACKAGE_VERSION%
 
-cmake -S . -B build -DCMAKE_INSTALL_PREFIX="%PACKIT_PACKAGE_PATH%"
+cmake -S build/cmake -B build -DCMAKE_INSTALL_PREFIX="%PACKIT_PACKAGE_PATH%"
 
 cmake --build build --config Release
 
--- packages/zstd/test.bat
+++ packages/zstd/test.bat
@@ -2,13 +2,13 @@ set TEST_TEXT = "Test test test I guess none of the many compression libraries i
 echo "%TEST_TEXT%" > test.txt
 
 REM Compress and decompress to see if information stays the same
-"%PACKIT_PACKAGE_PATH%\bin\zstd -c test.txt > compressed.zst
+"%PACKIT_PACKAGE_PATH%\bin\zstd" -c test.txt > compressed.zst
 if ERRORLEVEL 1 (
     echo Compression failed
     exit /b 1
 )
 
-"%PACKIT_PACKAGE_PATH%\bin\zstd -dc compressed.zst > decompressed.txt
+"%PACKIT_PACKAGE_PATH%\bin\zstd" -dc compressed.zst > decompressed.txt
 if ERRORLEVEL 1 (
     echo Decompression failed
     exit /b 1
