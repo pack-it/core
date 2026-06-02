00dfc08c03cf67a9ed977dbceb41df1c56cc9b06
Max Rijkers <max_rijkers1@icloud.com>
2026-06-02 16:41:11 +0200

    Add zstd package

---
+++ packages/zstd/build.bat
@@ -0,0 +1,9 @@
+cd zstd-%PACKIT_PACKAGE_VERSION%
+
+cmake -S . -B build -DCMAKE_INSTALL_PREFIX="%PACKIT_PACKAGE_PATH%"
+
+cmake --build build --config Release
+
+ctest --verbose -C Release
+
+cmake --install build
