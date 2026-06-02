9f4f1311e9c19d14fb6563c94da99e578e6aa2a1
Max Rijkers <max_rijkers1@icloud.com>
2026-06-02 21:28:19 +0200

    Add llhttp package

---
+++ packages/llhttp/test.bat
@@ -0,0 +1,35 @@
+REM Read Visual Studio install path
+for /f "tokens=* usebackq" %%i in (`"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere" -latest -property installationPath`) do (
+    set VSPATH=%%i
+)
+if not exist "%VSPATH%" (
+    echo Visual Studio cannot be loaded from %VSPATH%
+    exit /b 1
+)
+
+REM Check if vcvarsall.bat exists
+set "VCVARSALL=%VSPATH%\VC\Auxiliary\Build\vcvarsall.bat"
+if not exist "%VCVARSALL%" (
+    echo vcvarsall.bat cannot be loaded from %VCVARSALL%
+    exit /b 1
+)
+echo Found vcvarsall.bat at %VCVARSALL%
+
+REM Retrieve architecture from target
+if "%PACKIT_TARGET%"=="x86_64-pc-windows-msvc" (
+    set ARCH=x64
+) else if "%PACKIT_TARGET%"=="aarch64-pc-windows-msvc" (
+    set ARCH=arm64
+) else (
+    echo Target %PACKIT_TARGET% is not supported for this package
+    exit /b 1
+)
+
+REM Call vcvarsall.bat to set MSVC build environment
+call "%VCVARSALL%" %ARCH%
+
+cl /I "%PACKIT_PACKAGE_PATH%\include" test.c /Fe:test.exe /link /LIBPATH:"%PACKIT_PACKAGE_PATH%\lib" libllhttp.lib
+if ERRORLEVEL 1 exit /b %ERRORLEVEL%
+
+.\test.exe
+if ERRORLEVEL 1 exit /b %ERRORLEVEL%
---
+++ packages/llhttp/test.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+gcc -L "$PACKIT_PACKAGE_PATH/lib" -I "$PACKIT_PACKAGE_PATH/include" -Wl,-rpath,"$PACKIT_PACKAGE_PATH/lib" test.c -o test -lllhttp
+
+./test
