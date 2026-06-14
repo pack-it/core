72d2613b2c67ecf7300d1c24e40c0239cfec543b
Frank <mail@franku.nl>
2026-06-14 12:27:14 +0200

    Add pkg-config symlinks to pkgconf to use it as alternative

--- packages/pkgconf/build.sh
+++ packages/pkgconf/build.sh
@@ -6,3 +6,8 @@ cd pkgconf-$PACKIT_PACKAGE_VERSION
 make
 
 make install
+
+# Create pkg-config symlinks to use pkgconf as pkg-config alternative
+cd $PACKIT_PACKAGE_PATH
+ln -s pkgconf bin/pkg-config
+ln -s pkgconf.1 share/man/man1/pkg-config.1
