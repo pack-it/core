5a6607bbf92053c268f1c8834503704c5c95c323
Max Rijkers <max_rijkers1@icloud.com>
2026-07-29 11:22:26 +0200

    Add pcre2 test script

--- packages/pcre2/build.sh
+++ packages/pcre2/build.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 cd pcre2-$PACKIT_PACKAGE_VERSION
 
+extra_flags=""
+if [ "$PACKIT_OS" = "mac" ]; then
+    extra_flags="--enable-pcre2test-libedit"
+fi
+
 ./configure --prefix=$PACKIT_PACKAGE_PATH \
     --enable-pcre2-16 \
     --enable-pcre2-32 \
     --enable-pcre2grep-libz \
     --enable-pcre2grep-libbz2 \
-    --enable-jit \
-    --disable-dependency-tracking
+    --enable-jit="auto" \
+    --disable-dependency-tracking \
+    $extra_flags
 
 make
 
