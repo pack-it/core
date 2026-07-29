2994e60cd130d87ec3f689800f5c441915bb2abe
Max <max_rijkers1@icloud.com>
2026-07-29 12:50:50 +0200

    pcre2 for Linux

--- packages/pcre2/build.sh
+++ packages/pcre2/build.sh
@@ -1,19 +1,24 @@
 #!/bin/sh
 cd pcre2-$PACKIT_PACKAGE_VERSION
 
-extra_flags=""
+flags="--enable-pcre2-16 \
+        --enable-pcre2-32 \
+        --enable-pcre2grep-libz \
+        --enable-pcre2grep-libbz2 \
+        --enable-jit="auto" \
+        --disable-dependency-tracking"
+
 if [ "$PACKIT_OS" = "mac" ]; then
-    extra_flags="--enable-pcre2test-libedit"
+    ./configure --prefix=$PACKIT_PACKAGE_PATH \
+        $flags \
+        --enable-pcre2test-libedit
+else
+    ./configure --prefix=$PACKIT_PACKAGE_PATH \
+        $flags \
+        CPPFLAGS="-I$PACKIT_PACKAGE_DEPENDENCIES_PATH/zlib-ng-compat/include -I$PACKIT_PACKAGE_DEPENDENCIES_PATH/bzip2/include" \
+        LDFLAGS="-L$PACKIT_PACKAGE_DEPENDENCIES_PATH/zlib-ng-compat/lib -lz -L$PACKIT_PACKAGE_DEPENDENCIES_PATH/bzip2/lib -lbz2"
 fi
 
-./configure --prefix=$PACKIT_PACKAGE_PATH \
-    --enable-pcre2-16 \
-    --enable-pcre2-32 \
-    --enable-pcre2grep-libz \
-    --enable-pcre2grep-libbz2 \
-    --enable-jit="auto" \
-    --disable-dependency-tracking \
-    $extra_flags
 
 make
 
