b77c3a1c3f87beab15b729300f243c0347aa1d8c
Max <max_rijkers1@icloud.com>
2026-07-30 20:38:48 +0200

    Add mpdecimal for Windows

--- packages/mpdecimal/build.bat
+++ packages/mpdecimal/build.bat
@@ -1,13 +1,59 @@
 cd mpdecimal-%PACKIT_PACKAGE_VERSION%
 
-cmake -S . -B build -DCMAKE_INSTALL_PREFIX="%PACKIT_PACKAGE_PATH%"
-if ERRORLEVEL 1 exit /b %ERRORLEVEL%
+REM Read Visual Studio install path
+for /f "tokens=* usebackq" %%i in (`"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere" -latest -property installationPath`) do (
+    set VSPATH=%%i
+)
 
-cmake --build build --config Release
-if ERRORLEVEL 1 exit /b %ERRORLEVEL%
+if not exist "%VSPATH%" (
+    echo Visual Studio cannot be loaded from %VSPATH%
+    exit /b 1
+)
 
-ctest --verbose -C Release
-if ERRORLEVEL 1 exit /b %ERRORLEVEL%
+REM Check if vcvarsall.bat exists
+set "VCVARSALL=%VSPATH%\VC\Auxiliary\Build\vcvarsall.bat"
+if not exist "%VCVARSALL%" (
+    echo vcvarsall.bat cannot be loaded from %VCVARSALL%
+    exit /b 1
+)
+echo Found vcvarsall.bat at %VCVARSALL%
 
-cmake --install build
-if ERRORLEVEL 1 exit /b %ERRORLEVEL%
+REM Retrieve architecture from target
+if "%PACKIT_TARGET%"=="x86_64-pc-windows-msvc" (
+    set ARCH=x64
+) else if "%PACKIT_TARGET%"=="aarch64-pc-windows-msvc" (
+    set ARCH=arm64
+) else (
+    echo Target %PACKIT_TARGET% is not supported for this package
+    exit /b 1
+)
+
+REM Call vcvarsall.bat to set MSVC build environment
+call "%VCVARSALL%" %ARCH%
+
+set "MACHINE=x64"
+if "%PACKIT_TARGET%"=="aarch64-pc-windows-msvc" (
+    set "MACHINE=ansi64"
+)
+
+REM Go into C code libmpdec and compile it
+cd libmpdec
+copy /y Makefile.vc Makefile
+nmake MACHINE=%MACHINE% DEBUG=0
+
+REM Move files to the correct directory
+robocopy . "%PACKIT_PACKAGE_PATH%\include" mpdecimal.h
+if %ERRORLEVEL% GEQ 8 exit /b %ERRORLEVEL%
+robocopy . "%PACKIT_PACKAGE_PATH%\lib" libmpdec-4.0.1.lib libmpdec-4.0.1.dll libmpdec-4.0.1.dll.lib libmpdec-4.0.1.dll.exp
+if %ERRORLEVEL% GEQ 8 exit /b %ERRORLEVEL%
+
+REM Go into C++ code libmpdec and compile it
+cd ..\libmpdec++
+copy /y Makefile.vc Makefile
+nmake DEBUG=0
+
+REM Move files to the correct directory
+robocopy . "%PACKIT_PACKAGE_PATH%\include" decimal.hh
+if %ERRORLEVEL% GEQ 8 exit /b %ERRORLEVEL%
+robocopy . "%PACKIT_PACKAGE_PATH%\lib" libmpdec++-4.0.1.lib libmpdec++-4.0.1.dll libmpdec++-4.0.1.dll.lib libmpdec++-4.0.1.dll.exp
+if %ERRORLEVEL% GEQ 8 exit /b %ERRORLEVEL%
--- packages/mpdecimal/test.bat
+++ packages/mpdecimal/test.bat
@@ -29,7 +29,7 @@ REM Call vcvarsall.bat to set MSVC build environment
 call "%VCVARSALL%" %ARCH% %PACKIT_OUTPUTS% >&3
 
 REM Compile test.c
-cl /I "%PACKIT_PACKAGE_PATH%\include" test.c /Fe:test.exe /link /LIBPATH:"%PACKIT_PACKAGE_PATH%\lib" mpdec.lib %PACKIT_OUTPUTS% >&3 2>&3
+cl /I "%PACKIT_PACKAGE_PATH%\include" test.c /Fe:test.exe /link /LIBPATH:"%PACKIT_PACKAGE_PATH%\lib" libmpdec-%PACKIT_PACKAGE_VERSION%.lib %PACKIT_OUTPUTS% >&3 2>&3
 if ERRORLEVEL 1 exit /b %ERRORLEVEL%
 
 REM Execute test
