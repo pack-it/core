a4984b9ce50b369eb3772642aed3f772bc6f76a8
Max <max_rijkers1@icloud.com>
2026-07-30 21:11:32 +0200

    Add rpath when compiling

--- packages/mpdecimal/test.sh
+++ packages/mpdecimal/test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-gcc -L "$PACKIT_PACKAGE_PATH/lib" -I "$PACKIT_PACKAGE_PATH/include" test.c -o test -lmpdec
+gcc -L "$PACKIT_PACKAGE_PATH/lib" -I "$PACKIT_PACKAGE_PATH/include" -Wl,-rpath,"$PACKIT_PACKAGE_PATH/lib" test.c -o test -lmpdec
 
 ./test >&3
