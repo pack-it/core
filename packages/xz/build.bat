f981bb7d5a17492ad2fd46f2abf4a441f462c801
Max <max_rijkers1@icloud.com>
2026-07-31 13:46:47 +0200

    Different naming for static and shared

--- packages/xz/build.bat
+++ packages/xz/build.bat
@@ -1,25 +1,22 @@
 cd xz-%PACKIT_PACKAGE_VERSION%
 
-cmake -S . -B build -DCMAKE_INSTALL_PREFIX="%PACKIT_PACKAGE_PATH%" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF
+cmake -S . -B build-static -DCMAKE_INSTALL_PREFIX="%PACKIT_PACKAGE_PATH%" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF
 if ERRORLEVEL 1 exit /b %ERRORLEVEL%
 
-cmake --build build --config Release
+cmake --build build-static --config Release
 if ERRORLEVEL 1 exit /b %ERRORLEVEL%
 
-ctest --verbose -C Release
+cmake --install build-static --config Release
 if ERRORLEVEL 1 exit /b %ERRORLEVEL%
 
-cmake --install build --config Release
+cmake -S . -B build-shared -DCMAKE_INSTALL_PREFIX="%PACKIT_PACKAGE_PATH%" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON
 if ERRORLEVEL 1 exit /b %ERRORLEVEL%
 
-cmake -S . -B build -DCMAKE_INSTALL_PREFIX="%PACKIT_PACKAGE_PATH%" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON
-if ERRORLEVEL 1 exit /b %ERRORLEVEL%
-
-cmake --build build --config Release
+cmake --build build-shared --config Release
 if ERRORLEVEL 1 exit /b %ERRORLEVEL%
 
 ctest --verbose -C Release
 if ERRORLEVEL 1 exit /b %ERRORLEVEL%
 
-cmake --install build --config Release
+cmake --install build-shared --config Release
 if ERRORLEVEL 1 exit /b %ERRORLEVEL%
