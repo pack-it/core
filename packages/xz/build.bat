08aabe5f7fafd6662e431d6c347c45cc879f4187
Max <87335212+Max13245@users.noreply.github.com>
2026-08-03 22:39:22 +0200

    Fix xz: build shared libs on Windows (#58)
    * Build xz shared libs
    
    * Different naming for static and shared
    
    * xz: Use only a single build and copy liblzma.dll for tests
    
    * Use correct directory for dll copy
    
    ---------
    
    Co-authored-by: Frank <mail@franku.nl>

--- packages/xz/build.bat
+++ packages/xz/build.bat
@@ -1,12 +1,15 @@
 cd xz-%PACKIT_PACKAGE_VERSION%
 
-cmake -S . -B build -DCMAKE_INSTALL_PREFIX="%PACKIT_PACKAGE_PATH%" -DCMAKE_BUILD_TYPE=Release
+cmake -S . -B build -DCMAKE_INSTALL_PREFIX="%PACKIT_PACKAGE_PATH%" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON
 if ERRORLEVEL 1 exit /b %ERRORLEVEL%
 
 cmake --build build --config Release
 if ERRORLEVEL 1 exit /b %ERRORLEVEL%
 
-ctest --verbose -C Release
+REM Copy liblzma.dll to ensure tests work
+copy build\Release\liblzma.dll build\tests_bin\Release\liblzma.dll
+
+ctest --verbose -C Release --test-dir build
 if ERRORLEVEL 1 exit /b %ERRORLEVEL%
 
 cmake --install build --config Release
