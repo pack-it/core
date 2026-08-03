77d7d7574998263ad52d0c400f1de2c9f1e79577
Frank <mail@franku.nl>
2026-08-03 21:51:01 +0200

    xz: Use only a single build and copy liblzma.dll for tests

--- packages/xz/build.bat
+++ packages/xz/build.bat
@@ -1,22 +1,16 @@
 cd xz-%PACKIT_PACKAGE_VERSION%
 
-cmake -S . -B build-static -DCMAKE_INSTALL_PREFIX="%PACKIT_PACKAGE_PATH%" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF
+cmake -S . -B build -DCMAKE_INSTALL_PREFIX="%PACKIT_PACKAGE_PATH%" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON
 if ERRORLEVEL 1 exit /b %ERRORLEVEL%
 
-cmake --build build-static --config Release
+cmake --build build --config Release
 if ERRORLEVEL 1 exit /b %ERRORLEVEL%
 
-cmake --install build-static --config Release
+REM Copy liblzma.dll to ensure tests work
+copy Release\liblzma.dll liblzma.dll
+
+ctest --verbose -C Release --test-dir build
 if ERRORLEVEL 1 exit /b %ERRORLEVEL%
 
-cmake -S . -B build-shared -DCMAKE_INSTALL_PREFIX="%PACKIT_PACKAGE_PATH%" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON
-if ERRORLEVEL 1 exit /b %ERRORLEVEL%
-
-cmake --build build-shared --config Release
-if ERRORLEVEL 1 exit /b %ERRORLEVEL%
-
-ctest --verbose -C Release
-if ERRORLEVEL 1 exit /b %ERRORLEVEL%
-
-cmake --install build-shared --config Release
+cmake --install build --config Release
 if ERRORLEVEL 1 exit /b %ERRORLEVEL%
