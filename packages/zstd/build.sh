239eb7390ada87b92a7605683dc1f097b3ec08ee
Max <87335212+Max13245@users.noreply.github.com>
2026-08-12 13:14:07 +0200

    Specify the include directories and libs for lz4 and lzma (#69)

--- packages/zstd/build.sh
+++ packages/zstd/build.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 cd zstd-$PACKIT_PACKAGE_VERSION
 
+lib_extension=""
+if [ "$PACKIT_OS" = "mac" ]; then
+    lib_extension="dylib"
+elif [ "$PACKIT_OS" = "linux" ]; then
+    lib_extension="so"
+fi
+
 cmake -S build/cmake -B build -DCMAKE_INSTALL_PREFIX="$PACKIT_PACKAGE_PATH" \
     -DCMAKE_BUILD_TYPE=Release \
     -DBUILD_SHARED_LIBS=ON \
@@ -10,7 +17,10 @@ cmake -S build/cmake -B build -DCMAKE_INSTALL_PREFIX="$PACKIT_PACKAGE_PATH" \
     -DCMAKE_INSTALL_RPATH="$PACKIT_PACKAGE_PATH/lib" \
     -DZSTD_ZLIB_SUPPORT=ON \
     -DZSTD_LZMA_SUPPORT=ON \
+    -DLIBLZMA_INCLUDE_DIR="$PACKIT_PACKAGE_DEPENDENCIES_PATH/xz/include" \
     -DZSTD_LZ4_SUPPORT=ON \
+    -DLIBLZ4_INCLUDE_DIR="$PACKIT_PACKAGE_DEPENDENCIES_PATH/lz4/include" \
+    -DLIBLZ4_LIBRARY="$PACKIT_PACKAGE_DEPENDENCIES_PATH/lz4/lib/liblz4.$lib_extension" \
     -DCMAKE_CXX_STANDARD=11
 
 cmake --build build --config Release
