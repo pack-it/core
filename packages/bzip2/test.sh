7ec964d4210347ab82c2a2018fdd9528b630a041
Frank <mail@franku.nl>
2026-08-16 14:58:41 +0200

    bzip2: fix test scripts

--- packages/bzip2/test.sh
+++ packages/bzip2/test.sh
@@ -5,7 +5,7 @@ echo "$test_text" > test.txt
 
 # Compress and decompress to see if information stays the same
 $PACKIT_PACKAGE_PATH/bin/bzip2 -c test.txt > compressed.bz2
-$PACKIT_PACKAGE_PATH/bin/bzip2 -d compressed.bz2 > decompressed.txt
+$PACKIT_PACKAGE_PATH/bin/bzip2 -cd compressed.bz2 > decompressed.txt
 
 result=$(cat decompressed.txt)
 
