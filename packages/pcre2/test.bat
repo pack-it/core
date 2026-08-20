2992ef86dfc8ba5e6cf9386a0384c392703d9c21
Max <max_rijkers1@icloud.com>
2026-08-20 12:44:47 +0200

    Fix pcre2 linking on Windows

--- packages/pcre2/test.bat
+++ packages/pcre2/test.bat
@@ -6,5 +6,5 @@ cl /I "%PACKIT_PACKAGE_PATH%\include" test.c /Fe:test.exe /link /LIBPATH:"%PACKI
 if ERRORLEVEL 1 exit /b %ERRORLEVEL%
 
 REM Execute test
-.\test.exe
+.\test.exe %PACKIT_OUTPUTS% >&3
 if ERRORLEVEL 1 exit /b %ERRORLEVEL%
