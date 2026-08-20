aa1ed9042207e22ebad29925485d87127a99c844
Max <max_rijkers1@icloud.com>
2026-08-20 11:29:22 +0200

    fix sed replacement on linux

--- packages/json-c/build.sh
+++ packages/json-c/build.sh
@@ -11,10 +11,17 @@ cmake -S . -B build -DCMAKE_INSTALL_PREFIX="$PACKIT_PACKAGE_PATH" -DCMAKE_BUILD_
 cmake --build build --config Release
 
 # Replace echo with printf, because echo can use its arguments literally
-sed -i '' '13d' tests/test_json_parse_cli.test
-sed -i '' "13i\\
+if [ "$PACKIT_OS" = "mac" ]; then
+    sed -i '' '13d' tests/test_json_parse_cli.test
+    sed -i '' "13i\\
 printf \'%s\' \'\"tenant=blue\;note=CANARY_STACK_WINDOW_2026\;status=ok\"\' > file1.dat
 " tests/test_json_parse_cli.test
+elif [ "$PACKIT_OS" = "linux" ]; then
+    sed -i '13d' tests/test_json_parse_cli.test
+    sed -i "13i\\
+printf \'%s\' \'\"tenant=blue\;note=CANARY_STACK_WINDOW_2026\;status=ok\"\' > file1.dat
+" tests/test_json_parse_cli.test
+fi
 ctest --verbose -C Release --test-dir build
 
 cmake --install build --config Release
