45b6d0a284f74466119539f6557419dad3acdddc
Max Rijkers <max_rijkers1@icloud.com>
2026-08-26 21:24:47 +0200

    Fix libssh2 build tests

--- packages/libssh2/build.sh
+++ packages/libssh2/build.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+# Disable tests which require docker and sshd which don't work when `--disable-docker-tests` is enabled
+# Note that the sshd tests do work when docker tests are not disabled.
 ./configure --prefix=$PACKIT_PACKAGE_PATH \
     --disable-silent-rules \
     --disable-examples-build \
+    --disable-docker-tests \
+    --disable-sshd-tests \
     --with-openssl \
     --with-libz \
     --with-libz-prefix="$PACKIT_PACKAGE_DEPENDENCIES_PATH/zlib-ng-compat" \
