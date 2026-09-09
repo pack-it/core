6fba189526f292d6de6186dfe196028e23d78007
Mr.panda <dylanmartijn@gmail.com>
2026-09-09 15:04:23 +0200

    Update packages/lua/build.bat
    Co-authored-by: Max <87335212+Max13245@users.noreply.github.com>

--- packages/lua/build.bat
+++ packages/lua/build.bat
@@ -1,4 +1,3 @@
-cd lua-%PACKIT_PACKAGE_VERSION%
 
 REM Read Visual Studio install path
 for /f "tokens=* usebackq" %%i in (`"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere" -latest -property installationPath`) do (
