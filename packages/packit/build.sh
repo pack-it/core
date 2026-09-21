2c9eca76fd6510a5ec8698d4658d28082cae8796
Frank <46161394+BraveChicken1@users.noreply.github.com>
2026-09-21 21:42:34 +0200

    packit: fix issues because of inner directory detection (#92)
    * packit: fix issues because of inner directory detection
    
    * Fix windows executable suffix
    
    ---------
    
    Co-authored-by: Max <max_rijkers1@icloud.com>

--- packages/packit/build.sh
+++ packages/packit/build.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
+# Go up one level if we are in the `bin` directory and see the `pit` file
+if [ -e "pit" ]; then
+    cd ..
+fi
+
+# Go up one level if we are in the build directory and see the `bin` directory
+if [ -e "bin" ]; then
+    cd ..
+fi
+
 mkdir -p "$PACKIT_PACKAGE_PATH"
 cp -R "packit@$PACKIT_PACKAGE_VERSION-0-$PACKIT_TARGET/." "$PACKIT_PACKAGE_PATH"
