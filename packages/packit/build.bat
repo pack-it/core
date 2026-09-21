7bd5a8213c1efffe13a2f065383fe092d6be0b33
Frank <mail@franku.nl>
2026-09-21 19:49:09 +0200

    packit: fix issues because of inner directory detection

--- packages/packit/build.bat
+++ packages/packit/build.bat
@@ -1,2 +1,12 @@
+REM Go up one level if we are in the `bin` directory and see the `pit` file
+if exist "pit" (
+    cd ..
+)
+
+REM Go up one level if we are in the build directory and see the `bin` directory
+if exist "bin" (
+    cd ..
+)
+
 mkdir "%PACKIT_PACKAGE_PATH%"
 xcopy ".\packit@%PACKIT_PACKAGE_VERSION%-0-%PACKIT_TARGET%\*" "%PACKIT_PACKAGE_PATH%\" /E /I
