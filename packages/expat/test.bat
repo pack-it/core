ea0885fc288a28f20bdf41157708ff90edd8dc67
Max Rijkers <max_rijkers1@icloud.com>
2026-09-23 17:54:07 +0200

    Test file existance in CI

--- packages/expat/test.bat
+++ packages/expat/test.bat
@@ -4,10 +4,14 @@ call "%PACKIT_VCVARSALL%" %PACKIT_VCVARSALL_ARCH% %PACKIT_OUTPUTS% >&3
 cl /I "%PACKIT_PACKAGE_PATH%\include" test.c /Fe:test.exe /link /LIBPATH:"%PACKIT_PACKAGE_PATH%\lib" libexpat.lib %PACKIT_OUTPUTS% >&3 2>&3
 if ERRORLEVEL 1 exit /b %ERRORLEVEL%
 
-.\test.exe > .\output_file
+.\test.exe > output_file
 if ERRORLEVEL 1 exit /b %ERRORLEVEL%
 
-set /p "output="<".\output_file"
+if exist "output_file" (
+    echo SOMETHING WITH THAT NAME EXISTS
+)
+
+set /p "output="<"output_file"
 set "expected_output=The count of Numeria says one, two, three, four, five, the thing that comes after five. AAaahhh this happens everytimeee!!"
 
 if not "%output%"=="%expected_output%" (
