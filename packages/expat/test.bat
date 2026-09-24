e0c5d9f7c40bc7dbbac21119889913ec77f61349
Max Rijkers <max_rijkers1@icloud.com>
2026-09-24 17:45:15 +0200

    Test test.exe existence

--- packages/expat/test.bat
+++ packages/expat/test.bat
@@ -4,7 +4,15 @@ call "%PACKIT_VCVARSALL%" %PACKIT_VCVARSALL_ARCH% %PACKIT_OUTPUTS% >&3
 cl /I "%PACKIT_PACKAGE_PATH%\include" test.c /Fe:test.exe /link /LIBPATH:"%PACKIT_PACKAGE_PATH%\lib" libexpat.lib %PACKIT_OUTPUTS% >&3 2>&3
 if ERRORLEVEL 1 exit /b %ERRORLEVEL%
 
+if exist "test.exe" (
+    echo TEST EXECUTABLE EXISTS
+    dir test.exe
+)
+
 test.exe
+set "rc=%ERRORLEVEL%"
+echo TEST.EXE EXIT CODE=%rc%
+
 test.exe > output_file
 type output_file
 if ERRORLEVEL 1 exit /b %ERRORLEVEL%
